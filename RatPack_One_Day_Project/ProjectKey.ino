46da80792152b2c0c116343428fbe6234ad73e94
star8kid <2022younganthonyn@aaps.k12.mi.us>
2021-09-25 02:13:37 -0400

    Changed file names. Work on steps 2 and 3

--- RatPack_One_Day_Project/ProjectKey.ino
+++ RatPack_One_Day_Project/ProjectKey.ino
@@ -11,6 +11,15 @@
 
   Step One, define the strip in code:
 
+  Variables:
+
+  Type VariableName = VariableValue;
+
+  The semicolon let's the program know it has reached the end of a command
+
+  The first thing we need to do is define each of these variables. Some of them look
+  a little different, but their purpose is still the same. For example, here's our "setup" code:
+
   const int ledcount = 60;
   CRGB ledstrip [ledcount];
   static int hue = 0;
@@ -22,6 +31,8 @@
 
   Step Two, change the color:
 
+  *Explain functions here*
+
   static int hue = 0; <--- Change this number (from earlier) to change the color
   
   void ChangeColor(){
@@ -32,6 +43,8 @@
   }
 
   Step Three, do a rainbow and show them how:
+
+  *Explain loops here*
 
   void RainbowPattern(){
     int tempHue = 0;
@@ -46,6 +59,14 @@
     delay(10);
   }
 
+  Step Four, try something new!:
+
+  With the techniques that we taught, can else can you create on the LED strip?
+  Possible Ideas:
+  - Can you make it show two solid colors at once?
+  - Can you make it flash between colors rather than cycle?
+  - Can you modify the rainbow to show a smaller gradient instead?
+
 
 
 */
@@ -58,7 +79,7 @@ static int hue = 200;
 void setup() {
   // put your setup code here, to run once:
   FastLED.addLeds<NEOPIXEL, 6>(leds, ledcount);
-  SetUpLEDS();
+  // SetUpLEDS();
   // ChangeColor();
 }
 
@@ -67,9 +88,10 @@ void loop() {
 }
 
 // Step One Function
+// (Changed the number of leds to 5 due to voltage drop and loss of COM Port detection)
 void SetUpLEDS(){
   static uint8_t hue = 0;
-  for (int i = 0; i < ledcount; i++) {
+  for (int i = 0; i < 5; i++) {
     leds[i]=CHSV(0, 0, 255);
   }
   FastLED.show();
@@ -95,4 +117,22 @@ void SetUpLEDS(){
   //   FastLED.show();
   //   tempHue++;
   //   delay(10);
+  // }
+
+// Step Four (Example) Fuction
+  // void DottedPattern(){
+  //   int firstHue = 148;
+  //   int secondHue = 200;
+  //   while(true){
+  //     for (int i = 0; i < ledcount; i++){
+  //       leds[i] = ((i % 2) == 0) ? CHSV(firstHue,255,255) : CHSV(secondHue,255,255);
+  //     }
+  //     FastLED.show();
+  //     delay(1000);
+  //     for (int i = 0; i < ledcount; i++){
+  //       leds[i] = ((i % 2) == 0) ? CHSV(secondHue,255,255) : CHSV(firstHue,255,255);
+  //     }
+  //     FastLED.show();
+  //     delay(1000);
+  //   }
   // }
